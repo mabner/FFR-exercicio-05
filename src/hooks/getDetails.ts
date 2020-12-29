5ea4cd4fafcf68effabde99bf0caeb82a03b4db8
Marcos Leite <30819867+mabner@users.noreply.github.com>
2020-12-29 05:35:27 -0300

    React import test

--- src/hooks/getDetails.ts
+++ src/hooks/getDetails.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { MoviesService } from '../services/MoviesService';
 
 export interface Movie {
