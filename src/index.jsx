53283a7a5948a15604a4736c175cec09604fb9db
Marcos Leite <30819867+mabner@users.noreply.github.com>
2020-12-29 02:29:35 -0300

    Separates store and removes unused imports

--- src/index.jsx
+++ src/index.jsx
@@ -10,21 +10,15 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import { reducers } from './reducers';
+import Store from './store/Store';
 
 import App from './App';
 
-const store = createStore(reducers);
-
 const rootElement = document.getElementById('root');
 ReactDOM.render(
-	<Provider store={store}>
-			<BrowserRouter>
-				<App />
-			</BrowserRouter>
+	<Provider store={Store}>
+			<App />
 	</Provider>,
 	rootElement
 );
