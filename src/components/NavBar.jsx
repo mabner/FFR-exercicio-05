81ca1d89fb95ff52dc876d09183464bcc6a6a3e1
Marcos Leite <30819867+mabner@users.noreply.github.com>
2020-12-29 03:14:20 -0300

    Styled navigation bar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,15 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
 export const NavBar = () => {
 	return (
-		<nav className="navBar">
-			<Link to="/">
-				Home
-			</Link>
-			<Link to="/playlists">
-				Playlist
-			</Link>
-		</nav>
+		<NavBarWrapper>
+			<Link to="/">.:Home:.</Link>
+			<Link to="/playlists">.:Playlist:.</Link>
+		</NavBarWrapper>
 	);
 };
+
+export const NavBarWrapper = styled.div`
+	color: white;
+	background-color: hsl(197, 93%, 29%);
+	border: none;
+	border-radius: 4px;
+	padding: 10px 24px;
+	text-align: center;
+	font-size: 16px;
+	margin: 4px 2px;
+	opacity: 0.7;
+	transition: 0.3s;
+	display: inline-block;
+	text-decoration: none;
+	cursor: pointer;
+`;
